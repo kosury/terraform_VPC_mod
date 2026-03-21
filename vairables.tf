c2ad1b9c062c23b5ec78126d96a207463d476da8
kosury <sonvimadhu@gmail.com>
2026-03-21 11:18:13 +0530

    added subnet-public and AZs

--- vairables.tf
+++ vairables.tf
@@ -16,3 +16,13 @@ variable "igw_tags" {
   type = map 
   default = {}
 }
+
+##subnets
+variable "public_subnet_cidrs" {
+  type = list 
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "public_subnet_tags" {
+  default = {}
+  type = map
+}
